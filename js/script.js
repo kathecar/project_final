ae243ea619c2de3a3d6c4fa30f77f62e2af41b50
YVargas2024 <yulian.vargas@genstudents.org>
2025-01-12 00:04:37 -0500

    perfil

---
+++ js/script.js
@@ -0,0 +1,9 @@
+        // Función para mostrar el modal
+        function mostrarModal(modalId) {
+            document.getElementById(modalId).style.display = "block";
+        }
+
+        // Función para cerrar el modal
+        function cerrarModal(modalId) {
+            document.getElementById(modalId).style.display = "none";
+        }
